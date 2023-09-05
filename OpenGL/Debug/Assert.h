6cdf203a97cc479f5abcc10733c3c207dc46c4bb
Tajamul Husen <tajamulhusen58@gmail.com>
2023-09-06 00:19:14 +0530

    logging and assertion added

--- OpenGL/Debug/Assert.h
+++ OpenGL/Debug/Assert.h
@@ -1,1 +1,6 @@
 #pragma once
+
+#include "Log.h"
+
+
+#define GL_ASSERT(check, ...) { if(!(check)) { GL_LOG_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
--- OpenGL/Debug/Log.h
+++ OpenGL/Debug/Log.h
@@ -1,1 +1,12 @@
 #pragma once
+
+#include <spdlog/spdlog.h>
+
+// Logging macros
+#define GL_LOG_INFO(...)          spdlog::info(__VA_ARGS__)
+#define GL_LOG_WARN(...)          spdlog::warn(__VA_ARGS__)
+#define GL_LOG_ERROR(...)         spdlog::error(__VA_ARGS__)
+#define GL_LOG_TRACE(...)		  spdlog::trace(__VA_ARGS__)
+#define GL_LOG_CRITICAL(...)	  spdlog::critical(__VA_ARGS__)
+
+
